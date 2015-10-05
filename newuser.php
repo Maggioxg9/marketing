b18d802ffd1d36453b41d7efa555786bad851365
Zach Maggio <maggioxg9@aol.com>
2015-10-05 12:07:46 -0500

    auto

--- newuser.php
+++ newuser.php
@@ -16,43 +16,44 @@
 		</div>
 		<div id="nav">
 		</div>
-		<center><div class="itembox">
+		<center><div class="createusersbox">
 				<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-					<div class="logindetails">
+					<div class="createdetails">
 						<span class="formfont">Desired Username:</span>
-							<input class="textbox" type="text" name="username" size="20" maxlength="20">
+							<input class="textbox" type="text" name="username" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
+					<div class="createdetails">
 						<span class="formfont">New Password:</span>
-							<input class="textbox" type="password" name="password" size="20" maxlength="20">
+							<input class="textbox" type="password" name="password" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
-						<span class="formfont">Retype Password:</span>
-							<input class="textbox" type="password" name="repassword" size="20" maxlength="20">
+					<div class="createdetails">
+						<span class="formfont">Re-type Password:</span>
+							<input class="textbox" type="password" name="repassword" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
+					<div class="createdetails">
 						<span class="formfont">First Name:</span>
-							<input class="textbox" type="text" name="firstname" size="20" maxlength="20">
+							<input class="textbox" type="text" name="firstname" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
+					<div class="createdetails">
 						<span class="formfont">Last Name:</span>
-							<input class="textbox" type="text" name="lastname" size="20" maxlength="20">
+							<input class="textbox" type="text" name="lastname" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
+					<div class="createdetails">
 						<span class="formfont">E-Mail:</span>
-							<input class="textbox" type="text" name="email" size="40" maxlength="20">
+							<input class="textbox" type="text" name="email" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
+					<div class="createdetails">
 						<span class="formfont">Phone:</span>
-							<input class="textbox" type="text" name="phone" size="40" maxlength="20">
+							<input class="textbox" type="text" name="phone" size="30" maxlength="20">
 					</div>
-					<div class="logindetails">
+					<div> 
+						<br>
 					</div>
 					<div class="g-recaptcha" data-sitekey="6Ld-CA4TAAAAADicP5F1_KcIIBagpsNeE-TG7nip"></div>
 
 
 
-					<div id="loginbtn">
+					<div id="createbtn">
 						<br>
 						<input id="createbutton" type="submit" value="Create Account">
 					</div>
