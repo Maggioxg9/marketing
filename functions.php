81413e02e00a7f4a00abca566b106bd19acb3e75
root <root@localhost>
2015-10-22 15:02:30 -0500

    auto

--- functions.php
+++ functions.php
@@ -1,11 +1,30 @@
 <?php 
+	include 'functions.php';
+	session_start();
+	clearSessionBools();
+
+
+
+
 	function clearSessionBools(){
-		print_r($_SESSION);
 		unset($_SESSION['tmppostbool']);
 		unset($_SESSION['tmppostemail']);
 		unset($_SESSION['tmppostuserfname']);
 		unset($_SESSION['tmppostuserlname']);
 		unset($_SESSION['tmppostphone']);
 	}
+
+
+
+	if(isset($_SESSION['usr']) && isset($_SESSION['pswd'])){
+		//user logged, redirect to 404
+		header("Location: notfound.html");
+		exit();
+	}else{
+		//not logged in, redirect to login
+		header("Location: login.html");
+		exit();
+	}
+
 	
 ?>
